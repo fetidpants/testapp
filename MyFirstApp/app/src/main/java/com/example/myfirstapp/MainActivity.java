25f89a597e9abdecdbff9f147d76afe4304fdb94
fetidpants <%mZDp26Bq*c%8DjU>
2017-02-08 19:52:52 -0800

    first test of app

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "" + position,
                         Toast.LENGTH_SHORT).show();
             }
+
         });
     }
 
